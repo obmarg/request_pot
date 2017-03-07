0749dc3d28ea841b386d609c8aeff57b80f3f6eb
Graeme Coupar <grambo@grambo.me.uk>
2017-03-07 16:24:57 +0000

    Update travis & mix.exs to use Elixir > 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RequestPot.Mixfile do
   def project do
     [app: :request_pot,
      version: "0.3.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
